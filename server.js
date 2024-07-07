29968649b93257c1947d5ac83de3731d6e8ffc3c
danikk314 <145961172+danikk314@users.noreply.github.com>
2024-07-07 18:49:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI || 'dein_mongodb_atlas_connection_string';
+const MONGO_URI = process.env.MONGO_URI;
 
-// MongoDB Verbindung
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -20,37 +19,27 @@ mongoose.connect(MONGO_URI, {
   console.error('Error connecting to MongoDB Atlas', err);
 });
 
-// Einfaches Schema und Modell
 const stringSchema = new mongoose.Schema({
   value: String
 });
 
 const StringModel = mongoose.model('String', stringSchema);
 
-// Endpunkt zum Speichern eines neuen Strings
 app.post('/save-string', async (req, res) => {
   const { value } = req.body;
   try {
-    // Alle Einträge abrufen und sortieren
     let strings = await StringModel.find().sort({ _id: 1 });
-
-    // Prüfen, ob mehr als 4 Einträge vorhanden sind
     if (strings.length >= 5) {
-      // Ältesten Eintrag löschen
       await StringModel.findByIdAndDelete(strings[0]._id);
     }
-
-    // Neuen String hinzufügen
     const newString = new StringModel({ value });
     await newString.save();
-
     res.status(201).send('String saved successfully');
   } catch (error) {
     res.status(500).send('Error saving string');
   }
 });
 
-// Endpunkt zum Abrufen aller Strings
 app.get('/get-strings', async (req, res) => {
   try {
     const strings = await StringModel.find().sort({ _id: 1 });
@@ -60,8 +49,7 @@ app.get('/get-strings', async (req, res) => {
   }
 });
 
-// Statische Dateien aus dem 'public'-Verzeichnis servieren
-app.use(express.static('public'));
+app.use(express.static('NWeb-main'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
